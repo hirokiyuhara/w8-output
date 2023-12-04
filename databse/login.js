947e33f612b283e2f0b5d978a432831343ac8fd3
hirokiyuhara <146685461+hirokiyuhara@users.noreply.github.com>
2023-12-04 10:56:13 +0900

    一度Sign Inのボタンが押されたら次回以降、emailとpasswordが自動入力される機能をlocalStorageを用いて実装

---
+++ databse/login.js
@@ -0,0 +1,22 @@
+'use strict';
+window.onload = function () {
+  // フォーム要素を取得
+  const form = document.querySelector('form');
+  // emailとpasswordの入力欄を取得
+  const emailInput = document.getElementById('email');
+  const passwordInput = document.getElementById('password');
+
+  // サイト固有のキーを使用してLocalStorageから値を取得し、入力欄にセット
+  emailInput.value = localStorage.getItem('mySiteEmail') || '';
+  passwordInput.value = localStorage.getItem('mySitePassword') || '';
+
+  form.onsubmit = function () {
+    // サイト固有のキーに対応するデータのみをLocalStorageから削除
+    localStorage.removeItem('mySiteEmail');
+    localStorage.removeItem('mySitePassword');
+
+    // ローカルストレージに新しい値を設定
+    localStorage.setItem('mySiteEmail', emailInput.value);
+    localStorage.setItem('mySitePassword', passwordInput.value);
+  };
+};
